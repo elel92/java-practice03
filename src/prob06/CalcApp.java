46892d6c3dc9e415ef3854c63aa8f46276ae5633
BIT <BIT@DESKTOP-Q04GE57>
2019-08-08 17:22:24 +0900

    6번 완료

--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -8,32 +8,63 @@ public class CalcApp {
 		Scanner scanner = new Scanner(System.in);
 		
 		while( true ) {
-
-			System.out.print( ">> " );
+			System.out.print(">> ");
 			String expression = scanner.nextLine();
 			
-			if( "quit".equals( expression ) ) {
+			if("quit".equals(expression)) {
 				break;
 			}
 			
-			String[] tokens = expression.split( " " );
+			String[] tokens = expression.split(" ");
 			
-			if( tokens.length != 3 ) {
-				System.out.println( ">> 알 수 없는 식입니다.");
+			if(tokens.length != 3) {
+				System.out.println(">> 알 수 없는 식입니다.");
 				continue;
 			}
 			
-
-			
-			/*  코드를 완성 합니다 */
-			
-			
-			
-			
+			if(tokens[1].equals("+")) {
+				System.out.println(">> " + new Add(tokens[0], tokens[2]).ans);
+			} else if(tokens[1].equals("-")) {
+				System.out.println(">> " + new Sub(tokens[0], tokens[2]).ans);
+			} else if(tokens[1].equals("*")) {
+				System.out.println(">> " + new Mul(tokens[0], tokens[2]).ans);
+			} else if(tokens[1].equals("/")) {
+				System.out.println(">> " + new Div(tokens[0], tokens[2]).ans);
+			}
 		}
 		
 		scanner.close();
-
 	}
+}
 
+class Add {
+	int ans;
+	
+	Add(String a, String b) {
+		ans = Integer.parseInt(a) + Integer.parseInt(b);
+	}
+}
+
+class Sub {
+	int ans;
+	
+	Sub(String a, String b) {
+		ans = Integer.parseInt(a) - Integer.parseInt(b);
+	}
+}
+
+class Mul {
+	int ans;
+	
+	Mul(String a, String b) {
+		ans = Integer.parseInt(a) * Integer.parseInt(b);
+	}
+}
+
+class Div {
+	double ans;
+	
+	Div(String a, String b) {
+		ans = Integer.parseInt(a) / Integer.parseInt(b);;
+	}
 }
