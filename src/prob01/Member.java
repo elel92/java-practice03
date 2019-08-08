7f23ed8c723e3bdc1f75e44426971814f12ceef4
BIT <BIT@DESKTOP-Q04GE57>
2019-08-08 16:13:57 +0900

    1번 완료

--- src/prob01/Member.java
+++ src/prob01/Member.java
@@ -1,4 +1,31 @@
 package prob01;
 
 public class Member {
+	private String id;
+	private String name;
+	private int point;
+	
+	Member(String id, String name, int point) {
+		this.id = id;
+		this.name = name;
+		this.point = point;
+	}
+	
+	public String setName(String change_name) {
+		name = change_name;
+		
+		return name;
+	}
+	
+	public int setPoint(int change_point) {
+		point = change_point;
+		
+		return point;
+	}
+	
+	public String setId(String change_id) {
+		id = change_id;
+		
+		return id;
+	}
 }
